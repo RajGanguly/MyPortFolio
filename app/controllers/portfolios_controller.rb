2fd54cae876c7b88a28fc1ebece3868c910a735e
RAJARSHI GANGULY <rajarshiganguly@RAJARSHIs-MacBook-Pro.local>
2018-01-30 22:01:58 +0530

    Added new portfolio and edit portfolio links to index

--- app/controllers/portfolios_controller.rb
+++ app/controllers/portfolios_controller.rb
@@ -8,18 +8,35 @@ class PortfoliosController < ApplicationController
 	end	
 
 
-	 def create
-	    @portfolio = Portfolio.new(params.require(:portfolio).permit(:title, :subtitle, :body))
+	def create
+		@portfolio = Portfolio.new(params.require(:portfolio).permit(:title, :subtitle, :body))
 
+		respond_to do |format|
+		  if @portfolio.save
+		    format.html { redirect_to portfolios_path, notice: 'Portfolio was successfully created.' }
+		    format.json { render :show, status: :created, location: @blog }
+		  else
+		    format.html { render :new }
+		    format.json { render json: @portfolio.errors, status: :unprocessable_entity }
+		  end
+		end
+	end
+
+	def edit
+		@portfolio = Portfolio.find(params[:id])
+	end	
+
+	def update
+		@portfolio = Portfolio.find(params[:id])
 	    respond_to do |format|
-	      if @portfolio.save
-	        format.html { redirect_to portfolios_path, notice: 'Portfolio was successfully created.' }
-	        format.json { render :show, status: :created, location: @blog }
+	      if @portfolio.update(params.require(:portfolio).permit(:title, :subtitle, :body))
+	        format.html { redirect_to portfolios_path, notice: 'Blog was successfully updated.' }
+	        format.json { render :show, status: :ok, location: @blog }
 	      else
-	        format.html { render :new }
+	        format.html { render :edit }
 	        format.json { render json: @portfolio.errors, status: :unprocessable_entity }
 	      end
 	    end
-	  end
+	end	
 
 end
